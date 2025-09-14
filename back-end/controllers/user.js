c7bb1409611f8fdc8aca886ae6d1da638086d392
José Valencia <jodavasa9936@gmail.com>
2025-09-14 17:11:09 -0500

    first commit

--- back-end/controllers/user.js
+++ back-end/controllers/user.js
@@ -134,31 +134,28 @@ exports.login = async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({
-        message: "The email address you entered is not connected to an account",
+      res.status(400).json({
+        message: "the email addres you entered is not connected to an account",
       });
     }
-
     const check = await bcrypt.compare(password, user.password);
     if (!check) {
-      return res
-        .status(400)
-        .json({ message: "Invalid credentials. Please try again." });
+      res.status(400).json({
+        message: "Invalid credentials. Please try again.",
+      });
     }
-
     const token = generateToken({ id: user._id.toString() }, "7d");
-    return res.status(200).json({
+    res.send({
       id: user._id,
       username: user.username,
       picture: user.picture,
       first_name: user.first_name,
       last_name: user.last_name,
-      token,
+      token: token,
       verified: user.verified,
     });
   } catch (error) {
-    console.error("login error:", error);
-    return res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ message: error.message });
   }
 };
 
