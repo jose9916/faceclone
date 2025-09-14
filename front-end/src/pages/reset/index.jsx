83eb13a6bb87cad40b02531b6d74a8e662735ce1
José Valencia <jodavasa9936@gmail.com>
2025-09-14 06:06:41 -0500

    fourth commit

--- front-end/src/pages/reset/index.jsx
+++ front-end/src/pages/reset/index.jsx
@@ -4,7 +4,6 @@ import { useDispatch, useSelector } from "react-redux";
 import Cookies from "js-cookie";
 import { Form, Formik } from "formik";
 import { useState } from "react";
-import { LoginInput } from "../../components/inputs/loginInput";
 import { SearchAccount } from "./SearchAccount";
 import { SendEmail } from "./SendEmail";
 import { CodeVerification } from "./CodeVerification";
