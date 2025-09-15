831b70b447ab64082efc14325fe147b63c67e104
José Valencia <jodavasa9936@gmail.com>
2025-09-15 16:51:27 -0500

    last

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 3000,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 3000,
+    allowedHosts: ["triumphant-adaptation-production.up.railway.app"],
+  },
+});
