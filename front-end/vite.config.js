74ccccfade9f7ad5bef4db08f4b2f15dadacc70c
José Valencia <jodavasa9936@gmail.com>
2025-09-16 14:32:25 -0500

    add

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5175, // puerto para desarrollo local
+    port: 5173, // puerto para desarrollo local
   },
   preview: {
     port: process.env.PORT || 8080, // Railway asigna el puerto
