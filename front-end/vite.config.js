5438483e84d6daf3484e6ea438094467b1eab4c4
José Valencia <jodavasa9936@gmail.com>
2025-09-15 21:40:51 -0500

    vite js

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
-    port: process.env.PORT || 5173,
-  },
-  preview: {
-    host: "0.0.0.0",
-    port: process.env.PORT || 5173,
-    allowedHosts: ["triumphant-adaptation-production.up.railway.app"],
+    port: 5173, // Configura el puerto del servidor de desarrollo
   },
 });
